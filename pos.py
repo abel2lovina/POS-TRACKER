8207321782980054b6fb428ca213160bdb014fbb
Abel Istifanus <abel2micro@gmail.com>
2026-02-06 14:55:20 +0100

    Updated UI: added background and removed app.run block

--- pos.py
+++ pos.py
@@ -350,7 +350,7 @@ def update_machine(machine_id):
     return render_template('update_machine.html', machine=machine)
 
 
-if __name__=='__main__':
-    with app.app_context():
-        db.create_all()
-        app.run(debug=True)
+#if __name__=='__main__':
+ #   with app.app_context():
+  #      db.create_all()
+   #     app.run(debug=True)
